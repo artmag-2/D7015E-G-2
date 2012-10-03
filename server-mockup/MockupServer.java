71765cfd6d408fbdbc3ff5fcdc7f56a0d4f04196
José Ángel Fernández <onir@pub92-54.pub.ltu.se>
2012-10-03 12:33:16 +0200

    Print the packet in the standard output
    Included also the .jar file to autoexecute

--- server-mockup/MockupServer.java
+++ server-mockup/MockupServer.java
@@ -25,6 +25,8 @@ public class MockupServer {
             
             ServerSocket serv = new ServerSocket(port);
             System.out.println("Server created at port" + port + ".");
+            
+            ByteBuffer wrap;
 
             while (true) {
                 Socket c = serv.accept();
@@ -35,29 +37,50 @@ public class MockupServer {
                 
                 boolean read = true ;
                 
+                
+                
                 while (read){
-                    
+                 
                    byte[] cellId = new byte[4];
                    input.read(cellId,0,4);
+                   
+                   System.out.println("Begin of packet");
+                   
+                   int cellIdNumber = ByteBuffer.wrap(cellId).getInt();
+                   System.out.println("Celld Id:" + cellIdNumber);
                    
                    byte[] nodeId = new byte[4];
                    input.read(nodeId,0,4);
                    
+                   int nodeIdNumber = ByteBuffer.wrap(nodeId).getInt();
+                   System.out.println("Node Id:" + nodeIdNumber);
+                   
                    byte[] sideRoad = new byte[1];
                    input.read(sideRoad,0,1);
+                   
+                   byte sideRoadNumber = ByteBuffer.wrap(sideRoad).get();
+                   System.out.println("Side Road:" + sideRoadNumber);
                    
                    byte[] timeStamp = new byte[8];
                    input.read(timeStamp,0,8);
                    
+                   long timeStampNumber = ByteBuffer.wrap(timeStamp).getLong();
+                   System.out.println("Time Stamp:" + timeStamp);
+                   
                    byte[] size = new byte[4];
                    input.read(size,0,4);
                    
-                   //Get the size of the row data that we need to read.            
-                   ByteBuffer wrapped = ByteBuffer.wrap(size); //Big-endian by default
-                   int rawDataSize = wrapped.getInt(); 
+                   int rawDataSize = ByteBuffer.wrap(size).getInt(); 
+                   System.out.println("Raw Data Size:" + size);
                    
                    byte[] rawData = new byte[rawDataSize];
                    input.read(rawData,0, rawDataSize);
+                   
+                   for (byte i : rawData)
+                       System.out.print(i);
+                   
+                   System.out.println("");
+                   System.out.println("End of packet");
                    
                 }
                 
